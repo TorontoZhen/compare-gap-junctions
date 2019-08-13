3bbf328e4cf5a7652834fa307bd7a345264611a3
d2fong <d2fong@uwaterloo.ca>
2019-08-13 16:29:55 -0400

    change gap junction loading api to include the catmaid link

--- catmaid_api.py
+++ catmaid_api.py
@@ -1,6 +1,9 @@
-from config import token
+from config import token, project_id, stack_id
 from requests.auth import HTTPBasicAuth
 import requests
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 from neuron_info import is_neuron, is_neuron_class, nclass
 
@@ -16,71 +19,91 @@ class CatmaidApiTokenAuth(HTTPBasicAuth):
         return r
 
 
-class CatmaidApi:
-  def __init__(self, token):
-    self.auth = CatmaidApiTokenAuth(token)
+def get_catmaid_link(coordinates, project_id, stack_id):
+  x, y, z = coordinates
+  return 'https://catmaid.nemanode.org/?pid={}&zp={}&yp={}&xp={}&tool=tracingtool&sid0={}&s0=0'.format(project_id, z, y, x, stack_id)
 
-  def get_skeleton_id_to_neuron_name_map(self, project_id):
-    annotation_response = requests.post(
-      'https://catmaid.nemanode.org/' + str(project_id) + '/annotations/query-targets',
-      auth=CatmaidApiTokenAuth(token)
-    ).json()
-
-
-    skeleton_id_to_neuron_name_map = {}
-
-    for entity in annotation_response['entities']:
-
-      # sometimes entity name is a string formatted like: ['AVAR'] or 'AVAR?'
-      # remove brackets and '?' to get something like 'AVAR'
-      name = entity['name'].replace('[', '').replace(']', '').replace('?', '')
-
-      if entity['type'] != 'neuron':
-        continue
-
-      # discard if not a neuron name and not a neuron class
-      if not (is_neuron(name) or is_neuron_class(name) ):
-        continue
-
-      skid = entity['skeleton_ids'][0]
-
-      skeleton_id_to_neuron_name_map[skid] = name
-
-    return skeleton_id_to_neuron_name_map
-
-  def get_gap_junctions(self, project_id):
-
-    skid_to_name = self.get_skeleton_id_to_neuron_name_map(project_id)
-    connector_response = requests.post(
-      'https://catmaid.nemanode.org/' + str(project_id) + '/connectors/',
-      data={
-          'with_partners': True,
-          'relation_type': 'gapjunction_with'
-      },
-      auth=CatmaidApiTokenAuth(token)
-    ).json()
-
-    gap_junctions = set()
-
-    for synapse_id, partners in connector_response['partners'].items():
-      neuron1_id = partners[0][2]
-      neuron2_id = partners[1][2]
+def get_skeleton_id_to_neuron_name_map(token, project_id):
+  annotation_response = requests.post(
+    'https://catmaid.nemanode.org/' + str(project_id) + '/annotations/query-targets',
+    auth=CatmaidApiTokenAuth(token)
+  ).json()
 
 
-      if neuron1_id not in skid_to_name or neuron2_id not in skid_to_name:
-        continue
+  skeleton_id_to_neuron_name_map = {}
 
-      neuron1_name = skid_to_name[neuron1_id]
-      neuron2_name = skid_to_name[neuron2_id]
+  for entity in annotation_response['entities']:
 
-      nc1, _ = nclass(neuron1_name)
-      nc2, _ = nclass(neuron2_name)
+    # sometimes entity name is a string formatted like: ['AVAR'] or 'AVAR?'
+    # remove brackets and '?' to get something like 'AVAR'
+    name = entity['name'].replace('[', '').replace(']', '').replace('?', '')
 
-      n_classes = [nc1, nc2]
-      n_classes.sort()
+    if entity['type'] != 'neuron':
+      continue
 
-      gap_junctions.add((n_classes[0], n_classes[1]))
+    # discard if not a neuron name and not a neuron class
+    if not (is_neuron(name) or is_neuron_class(name) ):
+      continue
 
-    return gap_junctions
+    skid = entity['skeleton_ids'][0]
+
+    skeleton_id_to_neuron_name_map[skid] = name
+
+  return skeleton_id_to_neuron_name_map
 
 
+def get_gap_junctions_from_catmaid():
+  skid_to_name = get_skeleton_id_to_neuron_name_map(token, project_id)
+
+  connector_response = requests.post(
+    'https://catmaid.nemanode.org/' + str(project_id) + '/connectors/',
+    data={
+        'with_partners': True,
+        'relation_type': 'gapjunction_with'
+    },
+    auth=CatmaidApiTokenAuth(token)
+  ).json()
+
+
+  gap_junction_ids = {}
+  for connector in connector_response['connectors']:
+    gj_id, x, y, z, _, _, _, _, _ = connector
+
+    gap_junction_ids[gj_id] = {
+      'coordinates': (x, y, z)
+    }
+
+  gap_junctions = []
+  for gj_id, partners in connector_response['partners'].items():
+    neuron1_id = partners[0][2]
+    neuron2_id = partners[1][2]
+    gj_id = int(gj_id)
+
+    if neuron1_id not in skid_to_name or neuron2_id not in skid_to_name:
+      continue
+
+    neuron1_name = skid_to_name[neuron1_id]
+    neuron2_name = skid_to_name[neuron2_id]
+
+    nc1, _ = nclass(neuron1_name)
+    nc2, _ = nclass(neuron2_name)
+
+    n_classes = [nc1, nc2]
+    n_classes.sort()
+
+    if gj_id not in gap_junction_ids:
+      continue
+
+    coordinates = gap_junction_ids[gj_id]['coordinates']
+    gj = {}
+    gj['gj_id'] = gj_id
+    gj['n1_name'] = neuron1_name
+    gj['n2_name'] = neuron2_name
+    gj['n1_class'] = nc1
+    gj['n2_class'] = nc2
+    gj['class_set'] = (n_classes[0], n_classes[1])
+    gj['link'] = get_catmaid_link(coordinates, project_id, stack_id)
+
+    gap_junctions.append(gj)
+
+  return gap_junctions
---
+++ durbin_file_api.py
@@ -0,0 +1,76 @@
+from collections import defaultdict
+from neuron_info import nclass
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+durbin_data_path = './data/edgelist_durbin.txt'
+
+def get_gap_junctions_from_durbin():
+  edges = defaultdict(int)
+
+
+  gap_junctions = []
+  with open(durbin_data_path) as f:
+      for l in f:
+          pre, post, typ, dataset, synapses = l.strip().split('\t')
+
+          # Skip muscles as they were reannotated.
+          if post == 'mu_bod':
+              continue
+
+          synapses = int(synapses)
+          pre = pre.replace('DVB', 'PVT')
+          post = post.replace('DVB', 'PVT')
+
+          rev_type = {'Gap_junction': 'Gap_junction',
+                      'Send': 'Receive',
+                      'Receive': 'Send',
+                      'Send_joint': 'Receive_joint',
+                      'Receive_joint': 'Send_joint'}
+
+          key = (dataset, typ, pre, post)
+          rev_key = (dataset, rev_type[typ], post, pre)
+
+          edges[key] = max(edges[key], synapses)
+          edges[rev_key] = max(edges[rev_key], synapses)
+
+      # AIA -> ASK was very clearly missed in JSH.
+      edges[('JSH', 'Send', 'AIAL', 'ASKL')] = 1
+      edges[('JSH', 'Send', 'AIAR', 'ASKR')] = 1
+      # DVA -> ASK was very clearly missed in JSH.
+      edges[('JSH', 'Send', 'DVA', 'AIZL')] = 1
+      edges[('JSH', 'Send', 'DVA', 'AIZR')] = 1
+      # ADLL -> ASHL was very clearly missed in JSH
+      edges[('JSH', 'Send', 'ADLL', 'ASHL')] = 3
+      # ADE -> RMG is present in the MoW drawings, but missed by Durbin.
+      edges[('N2U', 'Send', 'ADEL', 'RMGL')] = 3
+      edges[('N2U', 'Send', 'ADER', 'RMGR')] = 2
+
+      for edge, synapses in edges.items():
+
+          dataset, typ, pre, post = edge
+
+          dataset = {'N2U': 'white_adult', 'JSH': 'white_l4'}[dataset]
+
+          if typ == 'Gap_junction':
+            nc1, _ = nclass(pre)
+            nc2, _ = nclass(post)
+            n_classes = [nc1, nc2]
+            n_classes.sort()
+
+            gj = {}
+            gj['gj_id'] = ''
+            gj['n1_name'] = pre
+            gj['n2_name'] = post
+            gj['n1_class'] = nc1
+            gj['n2_class'] = nc2
+            gj['class_set'] = (n_classes[0], n_classes[1])
+            gj['link'] = 'N/A'
+
+            gap_junctions.append(gj)
+
+  return gap_junctions
+
+pp.pprint(get_gap_junctions_from_durbin())
