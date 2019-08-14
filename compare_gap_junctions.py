186cffb085e6d8c7b874f06a1dc0eaf2b0650108
d2fong <d2fong@uwaterloo.ca>
2019-08-14 11:39:22 -0400

    proper output sorting and formatting

--- compare_gap_junctions.py
+++ compare_gap_junctions.py
@@ -26,52 +26,42 @@ gj_intersection = cgj_set & dgj_set
 durbin_unique = dgj_set - cgj_set
 christine_unique = cgj_set - dgj_set
 
-pp.pprint(sorted(cgj, key= lambda o: o['class_set'])[:20])
 
 # write the results to csv file
-SEM_UNIQUE_OUTPUT_FILE = open('./output/sem_unique.csv', 'w+')
+SEM_UNIQUE_OUTPUT_FILE = open('./output/christine_unique.csv', 'w+')
 DURBIN_UNIQUE_OUTPUT_FILE = open('./output/durbin_unique.csv', 'w+')
-INTERSECTION_OUTPUT_FILE = open('./output/intersection.csv', 'w+')
+
+SEM_UNIQUE_OUTPUT_FILE.write("{},{}\n".format('Unique class pairs', len(christine_unique)))
+SEM_UNIQUE_OUTPUT_FILE.write(',,,,,,\n')
+
+for gj_tuple in sorted(christine_unique, key=lambda tup: tup[0] + tup[1]):
+    SEM_UNIQUE_OUTPUT_FILE.write("{},{}\n".format(gj_tuple[0], gj_tuple[1]))
+    for gj_info in sorted(cgj, key= lambda o: o['class_set'][0]):
+        if gj_info['class_set'] == gj_tuple:
+            neurons = sorted([gj_info['n1_name'], gj_info['n2_name']])
+
+            pre = neurons[0]
+            post = neurons[1]
+            link = gj_info['link']
+            SEM_UNIQUE_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
+    SEM_UNIQUE_OUTPUT_FILE.write(',,,,,,\n')
 
 
-for gj_info in sorted(cgj, key= lambda o: o['class_set']):
-    class_set = gj_info['class_set']
-
-    pre = gj_info['n1_name']
-    post = gj_info['n2_name']
-    link = gj_info['link']
-    if class_set in gj_intersection:
-        INTERSECTION_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
-    elif class_set in christine_unique:
-        SEM_UNIQUE_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
-
+DURBIN_UNIQUE_OUTPUT_FILE.write("{},{}\n".format('Unique class pairs', len(durbin_unique)))
+DURBIN_UNIQUE_OUTPUT_FILE.write(',,,,,,\n')
 
 for gj_tuple in sorted(durbin_unique, key=lambda tup: tup[0] + tup[1]):
+    DURBIN_UNIQUE_OUTPUT_FILE.write("{},{}\n".format(gj_tuple[0], gj_tuple[1]))
+
     for gj_info in sorted(dgj, key = lambda o: o['class_set'][0]):
         if gj_info['class_set'] == gj_tuple:
-            pre = gj_info['n1_name']
-            post = gj_info['n2_name']
+            neurons = sorted([gj_info['n1_name'], gj_info['n2_name']])
+
+            pre = neurons[0]
+            post = neurons[1]
             link = gj_info['link']
             DURBIN_UNIQUE_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
-
-
-
-# for gj_tuple in sorted(christine_unique, key=lambda tup: tup[0] + tup[1]):
-#     for gj_info in sorted(cgj, key= lambda o: o['class_set'][0]):
-#         if gj_info['class_set'] == gj_tuple:
-#             pre = gj_info['n1_name']
-#             post = gj_info['n2_name']
-#             link = gj_info['link']
-#             SEM_UNIQUE_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
-
-# for gj_tuple in sorted(gj_intersection, key=lambda tup: tup[0] + tup[1]):
-    # for gj_info in sorted(cgj, key= lambda o: o['class_set'][0]):
-    #     if gj_info['class_set'] == gj_tuple:
-    #         pre = gj_info['n1_name']
-    #         post = gj_info['n2_name']
-    #         link = gj_info['link']
-    #         INTERSECTION_OUTPUT_FILE.write("{},{},{}\n".format(pre, post, link))
+    DURBIN_UNIQUE_OUTPUT_FILE.write(',,,,,,\n')
 
 DURBIN_UNIQUE_OUTPUT_FILE.close()
 SEM_UNIQUE_OUTPUT_FILE.close()
-INTERSECTION_OUTPUT_FILE.close()
